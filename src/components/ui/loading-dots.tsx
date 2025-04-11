5d7c6179bc510c7edc52c56535a6b63a5aa47941
tonazar <tonazar@gmail.com>
2025-04-11 23:25:13 +0530

    error fixed

--- src/components/ui/loading-dots.tsx
+++ src/components/ui/loading-dots.tsx
@@ -6,12 +6,12 @@ interface LoadingDotsProps {
 
 export function LoadingDots({ className }: LoadingDotsProps) {
   return (
-    <div className={className}>
+    <motion.div className={className}>
       <div className="flex space-x-1">
         <div className="w-2 h-2 bg-current rounded-full animate-[bounce_1s_infinite_100ms]" />
         <div className="w-2 h-2 bg-current rounded-full animate-[bounce_1s_infinite_200ms]" />
         <div className="w-2 h-2 bg-current rounded-full animate-[bounce_1s_infinite_300ms]" />
       </div>
-    </div>
+    </motion.div>
   );
 }
